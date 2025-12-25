d32f8db717ffb59dfd5c7445e1ed0362c7761c2c
Harshit Harsh <216764572+harshit-harsh659@users.noreply.github.com>
2025-12-25 17:25:22 +0530

    Create map.provider.js

---
+++ map.provider.js
@@ -0,0 +1,54 @@
+// map.provider.js
+const MapProvider = (() => {
+  let map, userMarker, destMarker, routeLayer;
+
+  function init(containerId, center, zoom) {
+    map = L.map(containerId).setView(center, zoom);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19
+    }).addTo(map);
+  }
+
+  function onClick(cb) {
+    map.on("click", e =>
+      cb({ lat: e.latlng.lat, lng: e.latlng.lng })
+    );
+  }
+
+  function setUserLocation(pos) {
+    if (!userMarker) {
+      userMarker = L.marker([pos.lat, pos.lng])
+        .addTo(map)
+        .bindPopup("You are here");
+    } else {
+      userMarker.setLatLng([pos.lat, pos.lng]);
+    }
+  }
+
+  function setDestination(pos) {
+    if (destMarker) map.removeLayer(destMarker);
+    destMarker = L.marker([pos.lat, pos.lng])
+      .addTo(map)
+      .bindPopup("Destination")
+      .openPopup();
+  }
+
+  function drawRoute(points) {
+    if (routeLayer) map.removeLayer(routeLayer);
+    routeLayer = L.polyline(points, {
+      color:"#35d07f",
+      weight:4
+    }).addTo(map);
+
+    map.fitBounds(routeLayer.getBounds(), { padding:[30,30] });
+  }
+
+  return {
+    init,
+    onClick,
+    setUserLocation,
+    setDestination,
+    drawRoute
+  };
+})();
